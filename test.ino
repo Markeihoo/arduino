5a5f4b93cde2b758c816d531932cf1f1ce6da52d
Markeihoo <134832418+Markeihoo@users.noreply.github.com>
2026-03-17 09:55:20 +0700

    start

---
+++ test.ino
@@ -0,0 +1,23 @@
+#include <ESP8266WiFi.h>
+#include <PubSubClient.h>
+
+#define BAUD_RATE 115200
+#define SSID ""
+#define PASSWORD ""
+#define MQTT_PORT 1883
+
+const char * NQTT_SERVER = "mqtt.netpie.io";
+const char * CLIENT_ID = "";
+const char * TOKEN = "";
+const char * SECRET = "";
+
+
+void setup() {
+
+
+}
+
+
+void loop(){
+
+}
